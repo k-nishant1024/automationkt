3387107651f5001fab43007d260eb50faf7584d2
siddhim <siddhimestry99@gmail.com>
2025-06-25 23:58:05 +0530

    add Login Page code

--- src/main/java/testcase/LoginPageTest.java
+++ src/main/java/testcase/LoginPageTest.java
@@ -2,13 +2,16 @@ package testcase;
 
 import org.openqa.selenium.WebDriver;
 import pageObject.LoginPage;
+import utility.Utility;
 
 public class LoginPageTest {
     LoginPage logPage = new LoginPage();
+    Utility util = new Utility();
 
     public void loginSuccess(){
         logPage.enterUsername("standard_user");
         logPage.enterPassword("secret_sauce");
         logPage.clickLoginButton();
+        util.getText();
     }
 }
--- src/main/java/utility/Utility.java
+++ src/main/java/utility/Utility.java
@@ -12,4 +12,7 @@ public class Utility extends BasePage {
     public WebElement getElements(String xpath){
         return  basePage.driver.findElement(By.xpath(xpath));
     }
+    public String getText(){
+        return "Successful";
+    }
 }
